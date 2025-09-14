68f9274904dc4289d8754150224b9171bda77330
Rafael Khazivaliev <rafael.khazivaliev@gmail.com>
2025-09-14 19:06:53 +0300

    feat: добавлены 6 блоков для проектов под основным изображением, и увеличение/уменьшение основного изображения для более живого эффекта сайта

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 // Массив с названиями файлов из папки images
+const slides = document.querySelector('.slide');
+
 const images = [
     'images/1.jpg',
     'images/2.jpg',
@@ -8,18 +10,16 @@ const images = [
     // добавь сюда остальные файлы
 ];
 
-const slideshow = document.querySelector('.slideshow');
-
 let currentIndex = -1; // текущее изображение
 
 function changeBackground() {
     let newIndex;
     do {
         newIndex = Math.floor(Math.random() * images.length);
-    } while (newIndex === currentIndex); // пока не будет отличаться от текущего
+    } while (newIndex === currentIndex);
 
     currentIndex = newIndex;
-    slideshow.style.backgroundImage = `url(${images[currentIndex]})`;
+    slides.style.backgroundImage = `url(${images[currentIndex]})`;
 }
 
 // сразу показываем первое изображение
